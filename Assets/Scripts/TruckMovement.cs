b8ebba2a4280f6d14ae81a95837f29405b9f1824
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-04-30 20:49:23 +0200

    👍 Add max range indicator

--- Assets/Scripts/TruckMovement.cs
+++ Assets/Scripts/TruckMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -6,10 +8,12 @@ public sealed class TruckMovement : MonoBehaviour
     [SerializeField] private Vehicle truck;
     [SerializeField] private CityMap cityMap;
     [SerializeField] private new Camera camera;
+    [SerializeField] private GameObject indicator;
 
     [CanBeNull] public event VoidEventHandler PathSelected;
 
     private Pathfinder pathfinder;
+    private readonly List<GameObject> spawnedIndicators = new();
 
     [CanBeNull] private VehicleMovement currentMovement;
 
@@ -29,7 +33,14 @@ public sealed class TruckMovement : MonoBehaviour
             currentMovement = null;
         }
 
-        if (tryFindTile(out var tile) && pathfinder.TryFindPath(truck.LogicalTile, tile, out var path))
+        if (spawnedIndicators.Count == 0)
+        {
+            spawnIndicators();
+        }
+
+        if (tryFindTile(out var tile)
+            && pathfinder.TryFindPath(truck.LogicalTile, tile, out var path)
+            && path.Length > 0)
         {
             truck.PreparePath(path.Clamped(truck.RangePerTurn));
         }
@@ -43,6 +54,7 @@ public sealed class TruckMovement : MonoBehaviour
     public VehicleMovement CommitMovement()
     {
         currentMovement = truck.CommitVehicleMovement();
+        destroyIndicators();
         return currentMovement;
     }
 
@@ -58,5 +70,48 @@ public sealed class TruckMovement : MonoBehaviour
 
         tile = default;
         return false;
+    }
+
+    private void spawnIndicators()
+    {
+        var front = new List<Vector3Int> { truck.LogicalTile };
+        var seen = new HashSet<Vector3Int> { truck.LogicalTile };
+        for (var i = 0; i <= truck.RangePerTurn; i++)
+        {
+            var newFront = new List<Vector3Int>();
+            foreach (var tile in front)
+            {
+                if (tile != truck.LogicalTile)
+                {
+                    spawnIndicator(tile);
+                }
+
+                foreach (var nb in DirectionHelpers.EnumerateDirections().Select(d => tile.Neighbour(d)))
+                {
+                    if (cityMap.IsValid(nb) && cityMap.TileAt(nb).IsRoad() && !seen.Contains(nb))
+                    {
+                        newFront.Add(nb);
+                        seen.Add(nb);
+                    }
+                }
+            }
+            front = newFront;
+        }
+    }
+
+    private void spawnIndicator(Vector3Int tile)
+    {
+        var ind = Instantiate(indicator, transform, true);
+        ind.transform.position = cityMap.TileToCenterWorld(tile);
+        spawnedIndicators.Add(ind);
+    }
+
+    private void destroyIndicators()
+    {
+        foreach (var go in spawnedIndicators)
+        {
+            Destroy(go);
+        }
+        spawnedIndicators.Clear();
     }
 }
